5cfdcfae943bde3ee1eeddfa0a452997eaf89a8a
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-24 11:05:22 -0400

    Modify add_box to use polygons

--- draw.py
+++ draw.py
@@ -38,22 +38,28 @@ def add_box( polygons, x, y, z, width, height, depth ):
     z1 = z - depth
 
     #front
-    add_edge(polygons, x, y, z, x1, y, z)
-    add_edge(polygons, x, y1, z, x1, y1, z)
-    add_edge(polygons, x1, y, z, x1, y1, z)
-    add_edge(polygons, x, y, z, x, y1, z)
+    add_polygon(polygons, x, y, z, x, y1, z, x1, y1, z)
+    add_polygon(polygons, x1, y1, z, x1, y, z, x, y, z)
 
     #back
-    add_edge(polygons, x, y, z1, x1, y, z1)
-    add_edge(polygons, x, y1, z1, x1, y1, z1)
-    add_edge(polygons, x1, y, z1, x1, y1, z1)
-    add_edge(polygons, x, y, z1, x, y1, z1)
+    add_polygon(polygons, x, y, z1, x, y1, z1, x1, y1, z1)
+    add_polygon(polygons, x1, y1, z1, x1, y, z1, x, y, z1)
 
-    #sides
-    add_edge(polygons, x, y, z, x, y, z1)
-    add_edge(polygons, x1, y, z, x1, y, z1)
-    add_edge(polygons, x, y1, z, x, y1, z1)
-    add_edge(polygons, x1, y1, z, x1, y1, z1)
+    #top
+    add_polygon(polygons, x1, y, z, x1, y, z1, x, y, z1)
+    add_polygon(polygons, x, y, z1, x, y, z, x1, y, z)
+
+    #bottom
+    add_polygon(polygons, x1, y1, z, x1, y1, z1, x, y1, z1)
+    add_polygon(polygons, x, y1, z1, x, y1, z, x1, y1, z)
+
+    #left
+    add_polygon(polygons, x, y1, z, x, y, z, x, y, z1)
+    add_polygon(polygons, x, y, z1, x, y1, z1, x, y1, z)
+
+    #right
+    add_polygon(polygons, x1, y1, z, x1, y, z, x1, y, z1)
+    add_polygon(polygons, x1, y, z1, x1, y1, z1, x1, y1, z)
 
 def add_sphere(polygons, cx, cy, cz, r, steps ):
     points = generate_sphere(cx, cy, cz, r, steps)
