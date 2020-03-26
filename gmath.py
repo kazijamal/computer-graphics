452de45cf60d9bc3666e6b7a6ac0aad9bf0997cb
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-26 10:53:11 -0400

    Add calculate_normal

--- gmath.py
+++ gmath.py
@@ -10,14 +10,21 @@ def normalize(vector):
 
 #Return the dot porduct of a . b
 def dot_product(a, b):
-    i = 0
     ans = 0
-    while i < 3:
+    for i in range(0,3):
         ans += a[i] * b[i]
-        i += 1
     return ans
 
 #Calculate the surface normal for the triangle whose first
 #point is located at index i in polygons
 def calculate_normal(polygons, i):
-    return None
+    p0 = polygons[i]
+    p1 = polygons[i+1]
+    p2 = polygons[i+2]
+    a = [p1[0]-p0[0], p1[1]-p0[1], p1[2]-p0[2]]
+    b = [p2[0]-p0[0], p2[1]-p0[1], p2[2]-p0[2]]
+    ans = [0, 0, 0]
+    ans[0] = a[1]*b[2] - a[2]*b[1]
+    ans[1] = a[2]*b[0] - a[0]*b[2]
+    ans[2] = a[0]*b[1] - a[1]*b[2]
+    return ans
