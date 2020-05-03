177c517c34c9ab6c5393bebb99db3012cea309b8
Kazi Jamal <jamalkazi02@gmail.com>
2020-05-03 17:21:31 -0400

    Change t to tmp

--- script.py
+++ script.py
@@ -57,25 +57,25 @@ def run(filename):
             stack.pop()
         elif op == 'move':
             args = command['args']
-            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
-            matrix_mult(stack[-1], t)
-            stack[-1] = [x[:] for x in t]
+            tmp = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(stack[-1], tmp)
+            stack[-1] = [x[:] for x in tmp]
         elif op == 'rotate':
             args = command['args']
             theta = float(args[1]) + (math.pi / 180)
             if args[0] == 'x':
-                t = make_rotX(theta)
+                tmp = make_rotX(theta)
             elif args[0] == 'y':
-                t = make_rotY(theta)
+                tmp = make_rotY(theta)
             else:
-                t = make_rotZ(theta)
-            matrix_mult(stack[-1], t)
-            stack[-1] = [x[:] for x in t]
+                tmp = make_rotZ(theta)
+            matrix_mult(stack[-1], tmp)
+            stack[-1] = [x[:] for x in tmp]
         elif op == 'scale':
             args = command['args']
-            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
-            matrix_mult(stack[-1], t)
-            stack[-1] = [x[:] for x in t]
+            tmp = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(stack[-1], tmp)
+            stack[-1] = [x[:] for x in tmp]
         elif op == 'box':
             args = command['args']
             constant = command['constants'] if command['constants'] else '.white'
